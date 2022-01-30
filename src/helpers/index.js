59b3d02abf222aa1a98da93176d943406d434c57
Ady <polcsek.dm90@gmail.com>
2022-01-30 19:09:43 +0100

    chore: add export changes

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,5 +2,7 @@ module.exports = {
     ErrorService: require('./error.service'),
     LoggerService: require('./logger.service'),
     QueryBuilderService: require('./query-builder.service'),
-    PayloadValidationService: require('./payload-validation.service'),
+    PayloadValidationService: require('./middlewares/payload-validation'),
+    Authentication: require('./middlewares/authentication'),
+    Authorization: require('./middlewares/authorization'),
 };
