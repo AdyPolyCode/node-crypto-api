1a3165af9b58b588c893e6f4459eafe11fabd573
Ady <polcsek.dm90@gmail.com>
2022-01-30 19:11:13 +0100

    chore: fix payload export

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,7 +2,7 @@ module.exports = {
     ErrorService: require('./error.service'),
     LoggerService: require('./logger.service'),
     QueryBuilderService: require('./query-builder.service'),
-    PayloadValidationService: require('./middlewares/payload-validation'),
+    PayloadValidation: require('./middlewares/payload-validation'),
     Authentication: require('./middlewares/authentication'),
     Authorization: require('./middlewares/authorization'),
 };
