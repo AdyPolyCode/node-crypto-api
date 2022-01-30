4647aa6f2f7bd1e79926d65ce4f0af7a4f2c5a46
Ady <polcsek.dm90@gmail.com>
2022-01-30 18:48:40 +0100

    chore: add shared file

---
+++ src/components/auth/schemas/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    forgotPassword: require('./forgot-password.schema'),
+    resetPassword: require('./reset-password.schema'),
+    login: require('./login.schema'),
+    register: require('./register.schema'),
+};
