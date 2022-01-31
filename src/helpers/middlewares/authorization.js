cf1f7c08d7b55174795777a3e83fa5fc7fc461bc
Ady <polcsek.dm90@gmail.com>
2022-01-31 18:15:37 +0100

    chore: add user role check

--- src/helpers/middlewares/authorization.js
+++ src/helpers/middlewares/authorization.js
@@ -1,1 +1,28 @@
-module.exports = class Authorization {};
+const { model } = require('mongoose');
+
+const { Forbidden } = require('../../errors');
+
+/* eslint-disable consistent-return */
+class Authorization {
+    constructor() {
+        this.authorize = this.#authorizeIt.bind(this);
+    }
+
+    #checkRole(role) {
+        const roles = ['admin', 'user'];
+
+        if (!role || !roles.includes(role)) {
+            return new Forbidden('You are not allowed to do this');
+        }
+    }
+
+    async #authorizeIt(req, res, next) {
+        const user = await model('User').findById(req.userId);
+
+        this.#checkRole(user.role);
+
+        next();
+    }
+}
+
+module.exports = new Authorization();
