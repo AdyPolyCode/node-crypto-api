3e97312a101f9cfd467ddf0e56461b2a104f3c3a
Ady <polcsek.dm90@gmail.com>
2022-01-31 18:10:50 +0100

    chore: add validation/auth mws

--- src/components/auth/auth.router.js
+++ src/components/auth/auth.router.js
@@ -1,16 +1,32 @@
 const Router = require('express').Router({ caseSensitive: true });
 
-// TODO: import + add validation/auth mws
-
 const AuthController = require('./auth.controller');
+const { PayloadValidation, Authentication } = require('../../helpers');
+const { login, register, forgotPassword, resetPassword } = require('./schemas');
 
 Router.post('/mail-confirmation/:mailToken', AuthController.confirmMail());
-Router.post('/password-reset/:resetToken', AuthController.resetPassword());
+Router.post(
+    '/password-reset/:resetToken',
+    PayloadValidation.validate(resetPassword),
+    AuthController.resetPassword()
+);
 
-Router.post('/forgot-password', AuthController.forgotPassword());
+Router.post(
+    '/forgot-password',
+    PayloadValidation.validate(forgotPassword),
+    AuthController.forgotPassword()
+);
 
-Router.post('/login', AuthController.login());
-Router.post('/register', AuthController.register());
-Router.get('/logout', AuthController.logout());
+Router.post(
+    '/login',
+    PayloadValidation.validate(login),
+    AuthController.login()
+);
+Router.post(
+    '/register',
+    PayloadValidation.validate(register),
+    AuthController.register()
+);
+Router.get('/logout', Authentication.authenticate, AuthController.logout());
 
 module.exports = Router;
