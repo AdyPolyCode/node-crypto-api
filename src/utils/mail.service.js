3d0d5eed614df3226b0657f62609df8311a869cc
Ady <polcsek.dm90@gmail.com>
2022-02-03 10:55:01 +0100

    chore: move props into constructor

--- src/utils/mail.service.js
+++ src/utils/mail.service.js
@@ -1,4 +1,3 @@
-module.exports = class MailService {};
 const nodemailer = require('nodemailer');
 const hbs = require('handlebars');
 const { readFileSync } = require('fs');
@@ -8,24 +7,29 @@ const MessageQueueService = require('./message-queue.service');
 const ConfigService = require('../config/config.service');
 const LoggerService = require('../helpers/logger.service');
 
-// TODO: change & finish hbs
+module.exports = class MailService {
+    #options;
 
-class MailService {
-    #options = {
-        'mail-confirmation': {
-            urlPath: '/api/auth/mail-confirmation',
-            templatePath: join(__dirname, 'templates/mail-confirmation.hbs'),
-            message:
-                'Please confirm this email so you can enjoy our services - ',
-        },
-        'password-reset': {
-            urlPath: '/api/auth/password-reset',
-            templatePath: join(__dirname, 'templates/password-reset.hbs'),
-            message: 'Here is your reset password email - ',
-        },
-    };
+    constructor() {
+        this.#options = {
+            'mail-confirmation': {
+                urlPath: '/api/auth/mail-confirmation',
+                templatePath: join(
+                    __dirname,
+                    'templates/mail-confirmation.hbs'
+                ),
+                message:
+                    'Please confirm this email so you can enjoy our services - ',
+            },
+            'password-reset': {
+                urlPath: '/api/auth/password-reset',
+                templatePath: join(__dirname, 'templates/password-reset.hbs'),
+                message: 'Here is your reset password email - ',
+            },
+        };
+    }
 
-    createUrl(type, token) {
+    #createUrl(type, token) {
         const port = ConfigService.getValue('NODE_PORT');
 
         const { urlPath } = this.#options[type];
@@ -33,23 +37,21 @@ class MailService {
         return `http://localhost${port}${urlPath}/${token}`;
     }
 
-    #createTemplate(type, url) {
+    #createTemplate(type, url, message) {
         const { templatePath } = this.#options[type];
 
         const file = readFileSync(templatePath, 'utf8');
 
-        return hbs.compile(file)({ URL: url });
+        return hbs.compile(file)({ URL: url, MESSAGE: message });
     }
 
     async mailIt(type, token, email) {
         try {
-            const url = this.createUrl(type, token);
+            const url = this.#createUrl(type, token);
 
-            const { message } = this.#options[type].concat(url);
+            const { message } = this.#options[type];
 
-            // TODO: add message to hbs template
-
-            const template = this.#createTemplate(type);
+            const template = this.#createTemplate(type, url, message);
 
             const transportOptions = ConfigService.loadNodeMailerConfig();
 
@@ -65,13 +67,13 @@ class MailService {
                 type
             );
 
-            await MessageQueueService.subscribe(type, transporter);
+            await MessageQueueService.subscribe(transporter, type);
+
+            return url;
         } catch (error) {
-            LoggerService.error(error.message);
+            LoggerService.error(error.stack);
 
             throw error;
         }
     }
-}
-
-module.exports = MailService;
+};
