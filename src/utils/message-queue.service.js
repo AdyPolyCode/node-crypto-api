df14bd4f488537b43d09a7fc40ccae78db70bf49
Ady <polcsek.dm90@gmail.com>
2022-02-03 10:55:35 +0100

    chore: fix issues/refactor

--- src/utils/message-queue.service.js
+++ src/utils/message-queue.service.js
@@ -3,50 +3,47 @@ const amqp = require('amqplib');
 const ConfigService = require('../config/config.service');
 
 module.exports = class MessageQueueService {
-    static async publish(payload, key) {
-        const connection = await amqp.connect(
-            ConfigService.getValue('RABBIT_URL')
-        );
+    static async publish(payload, type) {
+        const message = JSON.stringify(payload);
+
+        const url = ConfigService.getValue('RABBIT_URL');
+
+        const connection = await amqp.connect(url);
 
         const channel = await connection.createChannel();
 
-        await channel.assertExchange('base', 'direct', { durable: true });
+        const exchange = await channel.assertExchange('exOne', 'direct', {
+            durable: true,
+        });
 
-        await channel.publish('base', key, Buffer.from(payload), {
-            contentType: 'application/json',
+        await channel.publish(exchange.exchange, type, Buffer.from(message), {
             persistent: true,
         });
     }
 
-    static async subscribe(queue, transporter) {
-        const connection = await amqp.connect(
-            ConfigService.getValue('RABBIT_URL')
-        );
+    static async subscribe(transporter, type) {
+        const url = ConfigService.getValue('RABBIT_URL');
+
+        const connection = await amqp.connect(url);
 
         const channel = await connection.createChannel();
 
-        await channel.assertExchange('base', 'direct', { durable: true });
+        const queue = await channel.assertQueue('task_q', { durable: true });
 
-        await channel.assertQueue(queue, { durable: true });
+        await channel.bindQueue(queue.queue, 'exOne', type);
 
-        await channel.bindQueue(queue, 'base', 'direct');
+        await channel.consume(queue.queue, async (msg) => {
+            const data = JSON.parse(msg.content.toString('utf8'));
 
-        await channel.consume(
-            queue,
-            async (payload) => {
-                const data = JSON.parse(payload.content.toString('utf-8'));
+            try {
+                await transporter.sendMail(data);
 
-                try {
-                    await transporter.sendMail(data);
+                channel.ack(msg);
+            } catch (error) {
+                channel.nack(msg);
 
-                    channel.ack(payload);
-                } catch (error) {
-                    channel.nack(payload);
-
-                    throw error;
-                }
-            },
-            { noAck: false }
-        );
+                throw error;
+            }
+        });
     }
 };
