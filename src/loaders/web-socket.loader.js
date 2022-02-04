5c9abd46afea705dadc52f6ee45fe9b1bb1d38f5
Ady <polcsek.dm90@gmail.com>
2022-02-04 17:22:17 +0100

    chore: add logger service

--- src/loaders/web-socket.loader.js
+++ src/loaders/web-socket.loader.js
@@ -1,9 +1,17 @@
 const { Server } = require('socket.io');
 
+const { LoggerService } = require('../helpers');
+
 module.exports = (appServer) => {
     const io = new Server(appServer);
 
-    io.on('open', () => {});
+    io.on('open', () => {
+        LoggerService.info('Someone has connected...');
+    });
 
-    io.on('close', () => {});
+    io.on('close', () => {
+        LoggerService.info('Someone has left...');
+    });
+
+    return io;
 };
