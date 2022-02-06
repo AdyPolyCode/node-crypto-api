284f572ce5ad173f6453fe65000b3c9d512e7903
Ady <polcsek.dm90@gmail.com>
2022-02-06 20:42:36 +0100

    chore: add crypto route

--- src/loaders/express.loader.js
+++ src/loaders/express.loader.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const { ErrorService } = require('../helpers');
 
 const authRouter = require('../components/auth/auth.router');
-// ? const cryptoRouter = require('../components/crypto-token/crypto.router');
+const cryptoRouter = require('../components/crypto-token/crypto.router');
 
 module.exports = (app) => {
     app.use(
@@ -21,9 +21,11 @@ module.exports = (app) => {
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
 
+    /* eslint-disable import/no-extraneous-dependencies */
     if (process.env.NODE_ENV === 'dev') {
         app.use(require('morgan')('dev'));
     }
+    /* eslint-enable import/no-extraneous-dependencies */
 
     app.get('/test', async (req, res) => {
         res.json({
@@ -32,7 +34,7 @@ module.exports = (app) => {
     });
 
     app.use('/api/auth', authRouter);
-    // ? app.use('/api/crypto', cryptoRouter);
+    app.use('/api/crypto', cryptoRouter);
 
     app.use(ErrorService.handleNotFound);
     app.use(ErrorService.handleException);
