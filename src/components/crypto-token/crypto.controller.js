7c28f66114aa6452044915cf43fd9f2364d9a5c2
Ady <polcsek.dm90@gmail.com>
2022-02-10 19:30:15 +0100

    chore: implement controller for endpoints

--- src/components/crypto-token/crypto.controller.js
+++ src/components/crypto-token/crypto.controller.js
@@ -1,1 +1,30 @@
-class CryptoController {}
+const asyncHandler = require('express-async-handler');
+
+const CryptoService = require('./crypto.service');
+const { Factory, ServiceHandler } = require('../../helpers');
+
+class CryptoController extends ServiceHandler {
+    constructor(...services) {
+        super(services);
+    }
+
+    getMarkets() {
+        return asyncHandler(async (req, res) => {
+            const markets = await this.cryptoService.getMarkets(req.query);
+
+            res.json({ data: markets });
+        });
+    }
+
+    getCurrencyMetaData() {
+        return asyncHandler(async (req, res) => {
+            const currencies = await this.cryptoService.getCurrencyMetaData(
+                req.query
+            );
+
+            res.json({ data: currencies });
+        });
+    }
+}
+
+module.exports = Factory.create(CryptoController, CryptoService);
