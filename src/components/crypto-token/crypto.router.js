cedbdae80bcd9f8cf76dccea020b3f2451bbc1b5
Ady <polcsek.dm90@gmail.com>
2022-02-10 19:28:56 +0100

    chore: add getter endpoints

--- src/components/crypto-token/crypto.router.js
+++ src/components/crypto-token/crypto.router.js
@@ -1,3 +1,18 @@
-const Router = require('express').Router;
+const Router = require('express').Router({ caseSensitive: true });
+
+const CryptoController = require('./crypto.controller');
+const { QueryBuilder } = require('../../helpers');
+
+Router.get(
+    '/markets',
+    QueryBuilder.parseMarkets,
+    CryptoController.getMarkets()
+);
+
+Router.get(
+    '/currencies',
+    QueryBuilder.parseCurrencies,
+    CryptoController.getCurrencyMetaData()
+);
 
 module.exports = Router;
